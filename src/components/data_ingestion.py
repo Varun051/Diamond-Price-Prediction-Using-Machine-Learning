0cd96eb2c449180d570ce1fcf3681caff2463f6e
Varun Reddy <varunreddy051@gmail.com>
2025-07-05 19:29:42 +0530

    ModelTraining

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -57,9 +57,10 @@ if __name__=='__main__':
 '''
 
 ## run data transformation
-
+'''
 if __name__=='__main__':
     obj=DataIngestion()
     train_data_path,test_data_path=obj.initite_data_ingestion()
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+'''
